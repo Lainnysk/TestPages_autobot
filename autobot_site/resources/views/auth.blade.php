ac17a1784c64a78f127de1acdab6c414b473fabe
Ilya Gorev <isip_i.a.gorev@mpt.ru>
2022-03-24 21:05:00 +0300

    fix frog

--- autobot_site/resources/views/auth.blade.php
+++ autobot_site/resources/views/auth.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="\autobot_site\resources\css\app.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
     <title>Document</title>
 </head>
 <body>
 
 	<form action="action_page.php">
   <div class="imgcontainer">
-    <img src="C:\Users\Николай\Desktop\лягушка.jpg" alt="Avatar" class="avatar">
+    <img src="img/1.jpg" alt="Avatar" class="avatar">
   </div>
 
   <div class="container">
