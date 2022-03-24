1a2c31e8ae1aea76b72c71f836e49cc0eb0acdf3
Ilya Gorev <isip_i.a.gorev@mpt.ru>
2022-03-24 22:53:50 +0300

    fix sql

--- shared/pass_system.sql
+++ shared/pass_system.sql
@@ -7,24 +7,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Table `autobot_laravel`.`failed_jobs`
--- -----------------------------------------------------
-
-CREATE TABLE `failed_jobs` (
-  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uuid` VARCHAR(255) NOT NULL,
-  `connection` TEXT NOT NULL,
-  `queue` TEXT NOT NULL,
-  `payload` LONGTEXT NOT NULL,
-  `exception` LONGTEXT NOT NULL,
-  `failed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `failed_jobs_uuid_unique` (`uuid` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
 
 -- -----------------------------------------------------
 -- Table `autobot_laravel`.`messages`
@@ -34,57 +16,6 @@ CREATE TABLE `messages` (
   `message_id` VARCHAR(255) NOT NULL,
   `message_text` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`message_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `autobot_laravel`.`migrations`
--- -----------------------------------------------------
-
-CREATE TABLE `migrations` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `migration` VARCHAR(255) NOT NULL,
-  `batch` INT(11) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 12
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `autobot_laravel`.`password_resets`
--- -----------------------------------------------------
-
-CREATE TABLE  `password_resets` (
-  `email` VARCHAR(255) NOT NULL,
-  `token` VARCHAR(255) NOT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  INDEX `password_resets_email_index` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `autobot_laravel`.`personal_access_tokens`
--- -----------------------------------------------------
-
-CREATE TABLE  `personal_access_tokens` (
-  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tokenable_type` VARCHAR(255) NOT NULL,
-  `tokenable_id` BIGINT(20) UNSIGNED NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  `token` VARCHAR(64) NOT NULL,
-  `abilities` TEXT NULL DEFAULT NULL,
-  `last_used_at` TIMESTAMP NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `personal_access_tokens_token_unique` (`token` ASC) VISIBLE,
-  INDEX `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type` ASC, `tokenable_id` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
