1447f62c5cc12a1070006c51572c43927368fbfa
Ilya Gorev <isip_i.a.gorev@mpt.ru>
2022-04-05 16:50:11 +0300

    add essence

--- autobot_site/database/migrations/2022_04_05_125348_create_users_table.php
+++ autobot_site/database/migrations/2022_04_05_125348_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->integer('approved');
             $table->foreignId('id_role')->references('id_role')->on('roles');
             $table->foreignId('id_essence')->references('id_essence')->on('essences')->uniqid();
-            $table->foreignId('id_address')->references('id_address')->on('addreses');
+            $table->foreignId('id_address')->references('id_address')->on('addresses');
         });
     }
 
